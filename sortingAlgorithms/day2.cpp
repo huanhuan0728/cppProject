bd0906ff09af1da2924ab0830e94a7f1b0283204
xuhuan lu <1477405138@qq.com>
2025-02-08 22:04:16 +0800

    day2冒泡排序

--- sortingAlgorithms/day2.cpp
+++ sortingAlgorithms/day2.cpp
@@ -28,18 +28,38 @@ void shellSort(vector<int> &arr)
     for (int gap = n / 2; gap > 0; gap /= 2)
     {
         int i = gap;
-        for (i; i < n; i +=  gap)
+        for (i; i < n; i += gap)
         {
             int temp = arr[i];
             int j = i - gap;
-            while(j >=0 && arr[j] > temp)
+            while (j >= 0 && arr[j] > temp)
             {
-                arr[j+gap] = arr[j];
+                arr[j + gap] = arr[j];
                 j -= gap;
             }
 
             arr[j + gap] = temp;
-
         }
+    }
+}
+
+// 冒泡排序（Bubble Sort）
+void bubbleSort(vector<int> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; i < n - 1; i++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+            break;
     }
 }
