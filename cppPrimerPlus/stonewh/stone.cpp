cd6fb7b1637a3e0dd498455162b6e037aafb72e4
卢绪焕 <1477405138@qq.com>
2025-02-17 23:45:34 +0800

    stonewh

---
+++ cppPrimerPlus/stonewh/stone.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "stonewh.h"
+
+using namespace std;
+
+void display(const Stonewt &st, int n);
+
+int main()
+{
+    Stonewt incognito = 275;
+    Stonewt wolfe(287.7);
+    Stonewt taft(21, 8);
+
+    cout << "The celebrity weighed ";
+    incognito.show_stn();
+    cout << "The detective weighed ";
+    wolfe.show_stn();
+    cout << "The President weighed ";
+    taft.show_lbs();
+
+    incognito = 276.8;
+    taft = 245;
+    cout << "After dinner, thr celebrity weighed ";
+    incognito.show_lbs();
+    cout << "After dinner, the President weighed ";
+    taft.show_lbs();
+    display(taft, 2);
+
+    cout << "The wrestler weighed even more.\n";
+    display(422, 2);
+    cout << "No stone left unearned\n";
+    return 0;
+}
+
+void display(const Stonewt &st, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Wow!";
+        st.show_lbs();
+    }
+}
--- cppPrimerPlus/stonewh/stonewh.cpp
+++ cppPrimerPlus/stonewh/stonewh.cpp
@@ -15,3 +15,22 @@ Stonewt::Stonewt(int stn, double lbs)
     pds_left = lbs;
     pounds = stn * Lbs_per_stn + lbs;
 }
+
+Stonewt::Stonewt()
+{
+    stone = pounds = pds_left = 0;
+}
+
+Stonewt::~Stonewt()
+{
+}
+
+void Stonewt::show_stn() const
+{
+    cout << stone << "stone, " << pds_left << "pounds/n";
+}
+
+void Stonewt::show_lbs() const
+{
+    cout << pounds << "Pounds\n";
+}
