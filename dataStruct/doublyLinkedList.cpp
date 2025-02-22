460a595c0fece38f328fc56721e9946e89a7f866
卢绪焕 <1477405138@qq.com>
2025-02-22 23:41:00 +0800

    doublyLinkedList

--- dataStruct/doublyLinkedList.cpp
+++ dataStruct/doublyLinkedList.cpp
@@ -18,14 +18,15 @@ public:
     ~DoublyLinkedList();
 
     void insert_front(int val);
+    void push_front(int val);
     void push_back(int val);
     void insert(int val, int pos);
     void pop_front();
     void pop_back();
-    void remove(int pos);
+    void remove(int val);
     Node *find(int val);
-    void print_front();
-    void print_back();
+    void print_forward();
+    void print_backward();
 };
 
 DoublyLinkedList::~DoublyLinkedList()
@@ -40,6 +41,21 @@ DoublyLinkedList::~DoublyLinkedList()
 }
 
 void DoublyLinkedList::insert_front(int val)
+{
+    Node *newNode = new Node(val);
+    if (!head)
+    {
+        head = tail = newNode;
+    }
+    else
+    {
+        newNode->next = head;
+        head->prev = newNode;
+        head = newNode;
+    }
+}
+
+void DoublyLinkedList::push_front(int val)
 {
     Node *newNode = new Node(val);
     if (!head)
@@ -97,4 +113,130 @@ void DoublyLinkedList::insert(int val, int pos)
         current->next->prev = newNode;
     }
     current->next = newNode;
+}
+
+void DoublyLinkedList::pop_front()
+{
+    if (!head)
+    {
+        return;
+    }
+
+    Node *temp = head;
+    head = head->next;
+    if (head)
+    {
+        head->prev = nullptr;
+    }
+    else
+    {
+        tail = nullptr;
+    }
+    delete temp;
+}
+
+void DoublyLinkedList::pop_back()
+{
+    if (!head)
+    {
+        return;
+    }
+
+    Node *temp = tail;
+    tail = tail->prev;
+    if (tail)
+    {
+        tail->next = nullptr;
+    }
+    else
+    {
+        head = nullptr;
+    }
+}
+
+void DoublyLinkedList::remove(int val)
+{
+    Node *current = head;
+    while (current)
+    {
+        if (current->data == val)
+        {
+            if (current->prev)
+            {
+                current->prev->next = current->next;
+            }
+            else
+            {
+                head = current->next;
+            }
+
+            if (current->next)
+            {
+                current->next->prev = current->prev;
+            }
+            else
+            {
+                tail = current->prev;
+            }
+        }
+
+        current = current->next;
+    }
+}
+
+Node *DoublyLinkedList::find(int val)
+{
+    Node *current = head;
+    while (current)
+    {
+        if (current->data == val)
+        {
+            return current;
+        }
+        current = current->next;
+    }
+
+    return nullptr;
+}
+
+void DoublyLinkedList::print_forward()
+{
+    Node *current = head;
+    while (current)
+    {
+        std::cout << current->data << " ";
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+void DoublyLinkedList::print_backward()
+{
+    Node *current = tail;
+    while (current)
+    {
+        std::cout << current->data << " ";
+        current = current->prev;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    DoublyLinkedList dll;
+    dll.push_back(1);
+    dll.push_back(2);
+    dll.push_back(3);
+    dll.print_forward();
+    dll.pop_front();
+    dll.print_forward();
+    dll.push_front(0);
+    dll.print_forward();
+    dll.pop_back();
+    dll.print_forward();
+    dll.insert(4, 1);
+    dll.print_forward();
+    dll.remove(2);
+    dll.print_forward();
+    return 0;
 }
