894598674556a76469ee207a84c902c4e4e524fe
卢绪焕 <1477405138@qq.com>
2025-03-23 20:57:31 +0800

    leetcode20

---
+++ leetcode/_20.cpp
@@ -0,0 +1,45 @@
+#include <stack>
+#include <unordered_map>
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        std::stack<char> st;
+        for (char c : s)
+        {
+            // 如果是左括号
+            if (c == '(' || c == '{' || c == '[')
+                st.push(c);
+            else
+            {
+                if (st.empty())
+                    return false;
+
+                if (st.top() == '(')
+                {
+                    if (c == ')')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else if (st.top() == '{')
+                {
+                    if (c == '}')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else if (st.top() == '[')
+                {
+                    if (c == ']')
+                        st.pop();
+                    else
+                        return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
