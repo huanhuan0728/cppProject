4c65e2c46f26362765dfaedcfad5a37cd7cd9b9f
卢绪焕 <1477405138@qq.com>
2025-05-10 23:01:08 +0800

    leetcode242

---
+++ leetcode/_242.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// 242. Valid Anagram
+
+class Solution
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        int record[26] = {0};
+        for (int i = 0; i < s.size(); i++)
+        {
+            record[s[i] - 'a']++;
+        }
+        for (int i = 0; i < t.size(); i++)
+        {
+            record[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (record[i] != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
