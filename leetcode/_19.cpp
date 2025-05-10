f394290a38b72c8824c7de9299fd957c5289f72d
卢绪焕 <1477405138@qq.com>
2025-05-10 22:51:30 +0800

    leetcode19

---
+++ leetcode/_19.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        ListNode *dummyhead = new ListNode(0);
+        dummyhead->next = head;
+        ListNode *fast = head;
+        ListNode *slow = dummyhead;
+
+        while (n-- && fast)
+        {
+            fast = fast->next;
+        }
+        while (fast)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        slow->next = slow->next->next;
+
+        return dummyhead->next;
+    }
+};
