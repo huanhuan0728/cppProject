568d771332fd0b84e04dd1dde2f450c888da4248
卢绪焕 <1477405138@qq.com>
2025-05-15 22:18:40 +0800

    leetcode 144 145

---
+++ leetcode/_145.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <vector>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        travel(result, root);
+        return result;
+    }
+
+private:
+    void travel(vector<int> &arr, TreeNode *node)
+    {
+        if (node == nullptr)
+            return;
+
+        travel(arr, node->left);
+        travel(arr, node->right);
+        arr.push_back(node->val);
+    }
+};
